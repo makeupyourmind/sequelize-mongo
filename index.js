163f2edb2c15500cfa1f63a773c0bf298e58c104
makeupyourmind <nikitosnov@gmail.com>
2019-08-08 22:02:59 +0300

    added relations of sequelize

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const upload = require('./shared/multer');
-const User = require('./mongo/user_mongo');
+//const User = require('./mongo/user_mongo');
 const Profile = require('./mongo/profile');
 const { check } = require('express-validator');
 const validate = require('./middleware/checkError');
@@ -14,30 +14,28 @@ const {Service} = require('./Service');
 
 require('dotenv').config();
 
-const {Tester, sequelize} = require("./models");
+// const {Tester, Company, User, sequelize} = require("./models");
+let models = require("./models");
+const User = models.User;
+const Company = models.Company;
 
-sequelize.sync().then(function() {
+models.sequelize.sync().then(function() {
     console.log('Nice! Database looks fine')
 }).catch(function(err) {
     console.log(err, "Something went wrong with the Database Update!")
 });
 
-mongoose.connect('mongodb://localhost:27017/2', {useNewUrlParser: true}, function(err){
-  if(err){
-    console.log('Erro connect to DB', err);
-  }
-  console.log("Connected to DB");
-});
+// mongoose.connect('mongodb://localhost:27017/2', {useNewUrlParser: true}, function(err){
+//   if(err){
+//     console.log('Erro connect to DB', err);
+//   }
+//   console.log("Connected to DB");
+// });
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/uploads', express.static('uploads'));
-
-// app.post('/profile', function (req, res, next) {
-//     console.log("req.body : ", req.body);
-//     console.log(req.file);
-// });
 
 app.patch('/profilePhoto/:id', function (req, res) {
 
@@ -49,15 +47,6 @@ app.patch('/profilePhoto/:id', function (req, res) {
     Service.profilePhoto(req,res);
   })
 
-  // try {
-  //   if(req.file){
-  //     console.log("req.file", req.file);
-  //   }
-  //   // let response = await Profile.findOneAndUpdate({user: req.params.id}, req.file.patchname, {new: true});
-  //   // res.status(200).json({response});
-  // } catch (error) {
-  //   res.status(400).send(error.message);
-  // }
 });
 
 app.get('/getProfile/:id', async (req, res) => {
@@ -117,6 +106,45 @@ app.post('/',  [
       res.status(400).send(error.message);  
   }
 
+});
+
+app.post('/createUser', async (req, res) => {
+  try {
+    let response = await User.create(req.body);
+    res.json({response});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.post('/createCompany', async (req, res) => {
+  try {
+    let response = await Company.create(req.body);
+    res.json({response});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+
+app.get('/getUser', async(req, res) => {
+  try {
+    let response = await User.findOne({
+      where: {email: 'test'}, include: [{model: Company, as: 'company', where: { name: 'testCompany' }}]
+    })
+    res.json({response});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.get('/getCompany', async(req, res) => {
+try {
+  let response = await Company.findAll({include: ['employes']});
+  res.json({response});
+} catch (error) {
+  res.status(400).send(error.message);
+}
 });
 
 app.get('/', async (req, res) => {
--- models/index.js
+++ models/index.js
@@ -1,11 +1,50 @@
-const Sequelize = require('sequelize');
-const TesterModel = require('./tester');
+// const Sequelize = require('sequelize');
+// const TesterModel = require('./tester');
+// const CompanyModel = require('./company');
+// const UserModel = require('./user');
 
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres'
+// });
+
+// const Tester = TesterModel(sequelize, Sequelize);
+// const Company = CompanyModel(sequelize, Sequelize);
+// const User = UserModel(sequelize,Sequelize);
+
+// module.exports = {sequelize, Tester, Company, User};
+"use strict";
+ 
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || "development";
+var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+// var sequelize = new Sequelize(config.database, config.username, config.password, config);
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres'
 });
-
-const Tester = TesterModel(sequelize, Sequelize);
-
-module.exports = {sequelize, Tester};
+var db = {};
+ 
+ 
+fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+        return (file.indexOf(".") !== 0) && (file !== "index.js");
+    })
+    .forEach(function(file) {
+        var model = sequelize.import(path.join(__dirname, file));
+        db[model.name] = model;
+    });
+ 
+Object.keys(db).forEach(function(modelName) {
+    if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+    }
+});
+ 
+ 
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+module.exports = db;
